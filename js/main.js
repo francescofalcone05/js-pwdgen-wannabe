9a0d8eafd487a5d6071883c69144dfee654cfaf2
francescofalcone05 <ciccio_97@live.it>
2024-04-16 16:37:36 +0300

    pssw-generated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let datoUno = prompt ("Scrivi il tuo nome");
-let surname = prompt ("Scrivi il tuo cognome");
-let favColor = prompt ("Scrivi il tuo colore preferito");
+const datoUno = prompt ("Scrivi il tuo nome");
+const surname = prompt ("Scrivi il tuo cognome");
+const favColor = prompt ("Scrivi il tuo colore preferito");
 
-let newPassword = datoUno + surname + favColor;
+let newPassword = datoUno + surname + favColor + "#5";
 
-document.getElementById("titolo").innerHTML = newPassword;
+document.getElementById("mypssw").innerHTML = newPassword;
