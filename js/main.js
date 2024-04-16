89213e96f6742801b65b3070f8b78255bf2bda59
francescofalcone05 <ciccio_97@live.it>
2024-04-16 16:18:19 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const name = prompt ("Scrivi il tuo nome");
+const surname = prompt ("Scrivi il tuo cognome");
+const favColor = prompt ("Scrivi il tuo colore preferito");
+
+let newPassword = name + surname + favColor; 
+
+document.getElementById("titolo").innerHTML = surnamee;
